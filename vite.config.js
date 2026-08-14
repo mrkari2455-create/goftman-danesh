99a3dc270ad15796071571bdf62f8921b9f2251a
mrkari2455-create <mr.kari2455@gmail.com>
2026-08-15 00:18:14 +0330

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/goftman-danesh/',
 });
-google-site-verification: google3ea11f7947b79032.html
