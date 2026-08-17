5446611deda9ac1d60a055d5c2de32b711fb511d
mrkari2455-create <mr.kari2455@gmail.com>
2026-08-17 06:27:37 +0330

    Refactor vite.config.js to use export default

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-base: '/goftman-danesh/',
+export default defineConfig({
+  base: '/goftman-danesh/',
+  plugins: [react()],
+});
